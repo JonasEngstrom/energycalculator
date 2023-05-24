7b402d140018b3dc5aa3d61eaab99b8d28576e75
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2023-05-24 16:12:53 +0200

    Added a function to calculate BMI category weight limits.

---
+++ R/bmi_limits.R
@@ -0,0 +1,31 @@
+#' Calculate Body Mass Index Weight Limits
+#'
+#' Calculate the lower weight limits of body mass index categories based on body height in meters.
+#'
+#' @param height Body height in meters.
+#'
+#' @return A tibble with lower weight limits for each body mass index category.
+#' @export
+#'
+#' @examples
+#' bmi_limits(1.65)
+bmi_limits <- function(height) {
+  tibble::tibble(
+    bmi_category_lower_limit = c(
+      'Underweight',
+      'Normal weight',
+      'Pre-obesity',
+      'Obesity class I',
+      'Obesity class II',
+      'Obesity class III'
+    ),
+    weight = c(
+      0,
+      18.5,
+      25,
+      30,
+      35,
+      40
+    ) * height ^ 2
+  )
+}
---
+++ tests/testthat/test-bmi_limits.R
@@ -0,0 +1,23 @@
+test_that("bmi_limits returns correct weight values", {
+  expect_equal(
+    bmi_limits(1.65),
+    tibble::tibble(
+      bmi_category_lower_limit = c(
+        'Underweight',
+        'Normal weight',
+        'Pre-obesity',
+        'Obesity class I',
+        'Obesity class II',
+        'Obesity class III'
+      ),
+      weight = c(
+        0,
+        50.36625,
+        68.0625,
+        81.675,
+        95.2875,
+        108.9
+      )
+    )
+  )
+})
